ca45be2bd96cdf5c042d59c475536f68f59f96b3
akashdip2001 <akashdip.mahapatra.21@aot.edu.in>
2025-04-22 15:03:11 +0530

    update for Local Area Network uint IP

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const http = require('http');
-const socketio = require('socket.io');
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const os = require("os");
 
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public'))); // Use app.use here
+// Get local IP for LAN access
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return "localhost";
+}
 
-io.on('connection',function (socket)  {
-    socket.on("send-location", function (data) {
-    io.emit("receive-location", {id: socket.id, ...data});
-    });
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-    socket.on("disconnect", function() {
-        io.emit("user-disconnected", socket.id);
-    })
+io.on("connection", function (socket) {
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
 });
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+server.listen(PORT, HOST, () => {
+  console.log(`✅ Server is running at:`);
+  console.log(` - Local:   http://localhost:${PORT}`);
+  console.log(` - Network: http://${getLocalIP()}:${PORT}`);
 });
